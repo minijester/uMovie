3d90e3bc4476238b1074e799e40c1fd1f71ad87d
Warut Sathorn <rt.digital1234@gmail.com>
2020-10-06 17:53:19 +0700

    Initial commit

---
+++ app/src/main/java/com/miharu/umovie/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.miharu.umovie.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miharu.umovie.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
